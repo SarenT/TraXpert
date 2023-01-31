7b0fc437b6bd5543ebd75b85528d2a374e46cfa0
SarenT <sarentasciyan@hotmail.com>
2023-02-01 00:47:36 +0100

    refactor: :recycle: layout converted to ui/server design

--- server.R
+++ server.R
@@ -1,202 +1,3 @@
-#rm(list = ls())
-# Line below should run before other packages are loaded. See link for the reason:
-# https://stackoverflow.com/questions/27153974/how-to-fix-outofmemoryerror-java-gc-overhead-limit-exceeded-in-r
-
-if(version$major < 4){
-	cat("R version 4 is required.\nAborting...\n")
-	stopifnot(FALSE)
-}
-
-options(java.parameters = "-Xmx4096m")
-
-libs = c("methods", "xml2", "ggplot2", "magrittr", "dplyr", "ggpubr", "RColorBrewer", "udunits2", "sticky", "shiny",
-		 "sortable", "colourpicker", "DT", "svglite", "readxl", "colorspace", "smoother", "shinyBS", "car", "latex2exp")
-
-only.install = c("shinyjs", "htmlwidgets", "shinyWidgets")
-
-libs.func = c("dplyr", "tidyr", "purrr", "rlang", "viridisLite", "e1071", "circular")
-only.install.func = c("stringr", "ggiraphExtra", "Rmisc", "tools")
-
-all.required.libs = c(libs, only.install, libs.func, only.install.func)
-still.needed.libs = all.required.libs[!(all.required.libs %in% row.names(installed.packages()))]
-
-while(length(still.needed.libs)){
-	cat("Following packages are still missing: ", paste(still.needed.libs, collapse = ", "), "\n")
-	answer = readline(prompt="Do you want to automatically install these packages now? [(y)es, (n)o]: ")
-	if(answer == "y"){
-		install.packages(still.needed.libs)
-
-		still.needed.libs = all.required.libs[!(all.required.libs %in% row.names(installed.packages()))]
-	}else if(answer == "n"){
-		stop("Exiting...")
-	}else{
-		cat("Answer (", answer, ") not recognized only \"y\" for yes or \"n\" for no are allowed.\nExiting...\n")
-	}
-}
-
-# Install missing packages if there is any
-#install.packages(only.install.func[!is.element(only.install.func, installed.packages())])
-#install.packages(libs.func[!is.element(libs.func, installed.packages())])
-
-lapply(libs.func, require, character.only = TRUE)
-
-# Install missing packages if there is any
-#install.packages(only.install[!is.element(only.install, installed.packages())], )
-#install.packages(libs[!is.element(libs, installed.packages())])
-
-lapply(libs, require, character.only = TRUE)
-
-options(shiny.port = 7777, shiny.maxRequestSize=1024*1024^2, scipen = 10, browser = "firefox")
-
-# defaultWD = "~/"
-# if(dir.exists(defaultWD)){
-# 	setwd(defaultWD)	
-# }
-
-source("lib/constants.R")
-source("lib/functions.R")
-
-try(detach("package:plyr", unload=TRUE), silent = TRUE)
-
-# Define UI
-ui = function(request){
-	tabPanelImport = function(title = titleImportGroupings, tabColor){
-		tabPanel(title,
-				 tags$style(HTML(tabBGColorCSS(title, tabColor))),
-				 sidebarLayout(
-				 	sidebarPanel(#h3("TraXpert"),
-				 		#hr(),
-				 		p("TraXpert is a tool to automate analysis of tracking files to create plots and simple statistics. TraXpert is smart enough to understand basic file naming convention and the source of files, where each tracking file has ", 
-				 		  strong("\"_\""), 
-				 		  "separated groupings."),
-				 		br(),
-				 		p("For instance, files ",
-				 		  br(), #em("2019.05.08_WT_", strong("Untreated")),
-				 		  HTML("<em>2019.05.08_WT_<strong>Untreated</strong>.xml</em> and <br><em>2019.05.08_WT_<strong>Treated</strong>.xml</em> will be recognized as <br><em>2019.05.08_WT_<strong>group1</strong>.xml</em> and <br><em>2019.05.08_WT_<strong>group2</strong>.xml</em>. Also multiple groupings are supported (treatments, genotypes, etc...")
-				 		),
-				 		tags$ul(
-				 			tags$li("Select files with", strong("Choose...")),
-				 			tags$li("Set group labels and colors"),
-				 			tags$li("Click ", strong("Process Files"),  "Button for loading/parsing"),
-				 			tags$li(strong("Browse"), "the data or", strong("generate plots"), 
-				 					"via the tabs on the top right panel.")
-				 		),
-				 		fileInput(inputId = "tmFilesIn", label = "Choose files:", multiple = TRUE, 
-				 				  accept = c("text/xml", ".xml", ".xlsx", ".xls", ".csv", ".txt"), 
-				 				  placeholder = "Uploading...", buttonLabel = "Choose..."),
-				 		fluidPage(fluidRow(
-				 			column(7, actionButton(inputId = "processFilesIn", label = "Process Files")), 
-				 			column(3, checkboxInput("process_recalculate_In", label = "Recalculate", value = FALSE)),
-				 			column(2, checkboxInput("process_browse_In", label = "Debug", value = FALSE))
-				 		)),
-				 		p("OR"),
-				 		fileInput(inputId = "sessionFileIn", label = "Choose session file:", multiple = FALSE, 
-				 				  accept = c("text/tmx", ".tmx"), placeholder = "Uploading...", 
-				 				  buttonLabel = "Choose..."),
-				 		hr(),
-				 		downloadButton(outputId = "sessionOut", label = "Download session file"),
-				 		hr(),
-				 		selectInput("sampleIn", "Sample Files", c("None", list.files("res/")), selected = "None")),
-				 	mainPanel(
-				 		fluidPage(
-				 			fluidRow(column(12, DTOutput(outputId = "groupingsOut"))),
-				 			fluidRow(
-				 				column(6, tags$div(id = 'placeholder')),
-				 				column(
-				 					6, 
-				 					p("Rank groups accordingly. Groups will appear in plots according to these ranks."),
-				 					tags$div(id = 'placeholderRank'))
-				 			)
-				 		)
-				 	)
-				 )
-		)
-	}
-	
-	tabPanelOperations = function(title, tabColor){
-		tabPanel(title,
-				 tags$style(HTML(tabBGColorCSS(title, tabColor))),
-				 fluidPage(fluidRow( 
-				 	column(3, rotation_UI("rotation")),
-				 	column(9, point_source_UI("point_source"))
-				 )),
-				 fluidPage(fluidRow( 
-				 	feature_calculator_UI(
-				 		"track_new_feat", "New Track Feature", 
-				 		"Calculate a new track feature based on an existing feature. With this, you can calculate new measures by entering the formula.",
-				 		# "track", 
-				 		featureDimensionChoices, list(`Track` = "Track")),
-				 	feature_calculator_UI(
-				 		"traj_new_feat", "New Trajectory (Spot/Edge) Feature", 
-				 		"Calculate a new spot or edge feature based on an existing feature. With this, you can calculate new measures by entering the formula.",
-				 		# "traj", 
-				 		featureDimensionChoices, list(`Spot` = "Spot", `Edge` = "Edge")),
-				 	# )),
-				 	# fluidPage(fluidRow(
-				 	feature_calculator_UI(
-				 		"track_from_traj_new_feat", "New Track Feature from Trajectories", 
-				 		"Calculate a new track feature based on existing trajectory features. This summarises trajectory information (spots and edges of a track) into a single value per track. e.g. mean track speed is the average of all speeds between spots of a track.",
-				 		# "track_from_traj", 
-				 		featureDimensionChoices, list(`Track` = "Track"))
-				 ))
-				 
-		)
-	}
-	
-	fluidPage(shinyjs::useShinyjs(), shinyjs::extendShinyjs(functions = "shinyjs.init", 
-		text = paste0("shinyjs.init = function(){
-		  $('#tabs li a[data-value=", titleFiles ,"]').parents('li').hide(); $('#tabs li a[data-value=", 
-		  titleFeatures,"]').parents('li').hide();
-		  $('#tabs li a[data-value=", titleTracks ,"]').parents('li').hide(); $('#tabs li a[data-value=", 
-		  titleTrajectories,"]').parents('li').hide();
-		  $('#tabs li a[data-value=\"", 
-		  titlePlotTrackFeatures ,"\"]').parents('li').hide(); $('#tabs li a[data-value=\"", 
-		  titlePlotTrajectories,"\"]').parents('li').hide();
-		  $('#tabs li a[data-value=\"", 
-		  titlePlotDirectionality ,"\"]').parents('li').hide(); $('#tabs li a[data-value=\"", 
-		  titlePlotTrajFeatures,"\"]').parents('li').hide();
-		}")),
-			  title = titlePanel("TraXpert"),
-		tags$head(tags$style(HTML("hr {border-top: 1px solid #000000;} .action-button {background-color: #77FF77}
-								  .navbar-default .navbar-nav > li[class=active] {font-weight: bold;}"))),
-		fluidRow(
-			tags$head(tags$style(HTML(".tab-content { height: 90vh; overflow-y: auto !important; }"))),
-			#column(12,
-			navbarPage(title = "TraXpert", id = tabsID, 
-					   #tabsetPanel(type = "tabs",
-						tabPanelImport(title = titleImportGroupings, tabColor = tabColorImport),
-						tabPanelOperations(titleOperations, tabColorOperations),
-						tabPanel(titleFiles, 
-								 tags$style(HTML(tabBGColorCSS(titleFiles, tabColorTables))),
-								 table_output_UI("files_table_out")),
-						tabPanel(titleFeatures, 
-								 tags$style(HTML(tabBGColorCSS(titleFeatures, tabColorTables))),
-								 table_output_UI("features_table_out")),
-						tabPanel(titleTracks, 
-								 tags$style(HTML(tabBGColorCSS(titleTracks, tabColorTables))),
-								 table_output_UI("tracks_table_out")),
-						tabPanel(titleTrajectories, 
-								 tags$style(HTML(tabBGColorCSS(titleTrajectories, tabColorTables))),
-								 table_output_UI("trajectories_table_out")),
-						track_features_UI("track_features", titlePlotTrackFeatures, tabColorPlots),
-						trajectories_UI("trajectories", titlePlotTrajectories, tabColorPlots),
-						directionality_UI("directionality", titlePlotDirectionality, tabColorPlots),
-						trajectory_features_UI("trajectory_features", titlePlotTrajFeatures, tabColorPlots),
-						about_UI("about", titleAbout)
-					)
-			#)
-		),
-		fluidRow(
-			column(12, 
-				   fluidPage(
-				   	fluidRow( style="background-color:#eee; border: 1px solid #aaa; padding: 10px;border-radius:4px",
-				   			  column(4, align = "left", "by Saren Tasciyan"),
-				   			  column(4, align = "center", "All rights reserved"),
-				   			  column(4, align = "right", "v1.0")))
-			)
-		)
-	)
-}
 server = function(input, output, session) {
 	# source("functions.R")
 	
@@ -339,7 +140,7 @@ server = function(input, output, session) {
 	parseParameters = reactiveVal(NULL)
 	
 	observeEvent(input$processFilesIn, {
-	#dataTMFiles = eventReactive(input$processFilesIn, {
+		#dataTMFiles = eventReactive(input$processFilesIn, {
 		# Create a Progress object
 		recalculate = input$process_recalculate_In
 		browse = input$process_browse_In
@@ -386,7 +187,7 @@ server = function(input, output, session) {
 			updateProgress = function(value, detail = NULL) {
 				if(is.null(detail)){progress$set(value = value)}else{progress$set(value = value, detail = detail)}
 			}
-				
+			
 			closeProgress = function(){progress$close()}
 			
 			groupingsDF = groupings()$groupings
@@ -395,9 +196,9 @@ server = function(input, output, session) {
 				   endsWith(uploadedFiles$datapath, suffix = ".txt"))){
 				parseParameters(list(files = uploadedFiles$datapath, groupings = groupingsDF, groups = groupsDF, 
 									 fileNames = uploadedFiles$name, 
-					 updateProgress = updateProgress, closeProgress = closeProgress, 
-					 initializeProgress = initializeProgress, 
-					 browse = browse))
+									 updateProgress = updateProgress, closeProgress = closeProgress, 
+									 initializeProgress = initializeProgress, 
+									 browse = browse))
 				observeEvent(input$userUnitInputOKIn, {
 					# Check that data object exists and is data frame.
 					# browser()
@@ -487,7 +288,7 @@ server = function(input, output, session) {
 	groupingsAndFeatureChoiceswithoutEmpty = reactive({
 		#browser()
 		choices = list(`Grouping Variables` = as.vector(groupingsChoiceswithoutEmpty()),
-				`Measures` = as.vector(trajChoiceswithoutEmpty()))
+					   `Measures` = as.vector(trajChoiceswithoutEmpty()))
 		choices
 	})
 	
@@ -525,7 +326,7 @@ server = function(input, output, session) {
 		#choicesInNamedList(c("Spot", "Edge"), inp, empty = FALSE)
 	})
 	
-
+	
 	
 	observeEvent(input$groupingsOut_cell_clicked, {
 		info = input$groupingsOut_cell_clicked
@@ -542,7 +343,7 @@ server = function(input, output, session) {
 			removeUI(selector = '#placeholder textarea', multiple = TRUE)
 			removeUI(selector = '#placeholder label', multiple = TRUE)
 			id = paste0("groupLabel_", info$row)
-
+			
 			insertUI(selector = '#placeholder', ui = textInput(id, label = "Grouping label:", value = groupingLabel))
 			for(labelRV in labelRVs){labelRV$destroy()}
 			print(labelRVs)
@@ -558,7 +359,7 @@ server = function(input, output, session) {
 				groupings(list(groupings = groupingsDF, groups = groupsDF))
 			})
 			lapply(1:length(groups), function(i){
-			#for(i in 1:length(groups)){
+				#for(i in 1:length(groups)){
 				insertUI(selector = '#placeholder',
 						 ui = textAreaInput(paste0('label_', sprintf("%03d", i)), 
 						 				   label = paste0("Group label for ", groups[i], ":"), value = groupLabels[i]))
@@ -571,7 +372,7 @@ server = function(input, output, session) {
 						#cat(paste("---", id, info$row, "\n"))
 						#cat(paste("---", "\t", input[[paste0('label_', sprintf("%03d", i))]], paste(groupingsDF[info$row, 4], collapse = "-"), "\n"))
 						groupings(list(groupings = groupingsDF, groups = groupsDF))
-				})
+					})
 				insertUI(selector = "#placeholder", 
 						 ui = colourInput(paste0('color_', sprintf("%03d", i)), 
 						 				 label = paste0("Group color for ", groups[i], ":"), 
@@ -585,7 +386,7 @@ server = function(input, output, session) {
 						#cat(paste("+++", id, info$row, "\n"))
 						#cat(paste("+++", "\t", input[[paste0('color_', sprintf("%03d", i))]], paste(groupingsDF[info$row, 5], collapse = "-"), "\n"))
 						groupings(list(groupings = groupingsDF, groups = groupsDF))
-				})
+					})
 			})
 			print(names(labelRVs))
 			cat("========================================================\n")
@@ -611,17 +412,17 @@ server = function(input, output, session) {
 			for(rankListOb in rankListObs){rankListOb$destroy()}
 			rankListObs <<- list()
 			if(length(dataList) > 1){
-			
+				
 				lapply(1:nrow(groupingsDF), function(i){
-				#for(i in 1:nrow(groupingsDF)){
+					#for(i in 1:nrow(groupingsDF)){
 					#browser()
 					
 					#uiList[[i]] = 
 					#insertUI(selector = "#placeholderRank", ui =  textAreaInput(inputId = paste("rankGroupahaha",  sprintf("%03d", i), sep = "_"), 
 					#															label = "Grouping label:", value = paste(i)))
 					insertUI(selector = "#placeholderRank", ui = rank_list(text = as.character(groupingsDF$labels[i]),
-							  labels = as.list(as.character(groupingsDF$groupLabels[[i]])),
-							  input_id = paste("rankGroup", sprintf("%03d", i), sep = "_")), where = "beforeEnd")
+																		   labels = as.list(as.character(groupingsDF$groupLabels[[i]])),
+																		   input_id = paste("rankGroup", sprintf("%03d", i), sep = "_")), where = "beforeEnd")
 					cat(i, 
 						as.character(groupingsDF$labels[i]),  paste("rankGroup", sprintf("%03d", i), sep = "_"), 
 						"\n")
@@ -681,14 +482,14 @@ server = function(input, output, session) {
 				 colnames = c('Groupings', 'Grouping Labels', 'Groups', 'Group Labels', 'Group Colors'))
 	
 	output$files_point_source_Out = renderDT(data()$files, rownames = FALSE, 
-										  editable = list(target = c("cell"), disable = list(columns = c(0))), 
-										  selection = 'none', 
+											 editable = list(target = c("cell"), disable = list(columns = c(0))), 
+											 selection = 'none', 
 											 options = list(dom = 't', autoWidth = TRUE, 
 											 			   columnDefs = list(list(visible = FALSE, 
 											 			   					   targets = pointSourceColumnDisable())
-											 			   				  )
 											 			   )
-											 )#c(1, 2, 3))))
+											 )
+	)#c(1, 2, 3))))
 	
 	observeEvent(input$files_point_source_Out_cell_edit, {
 		#browser()
@@ -802,4 +603,4 @@ server = function(input, output, session) {
 	directionality_server("directionality", data, features, tracks, trajectories, groupings, choices)
 	trajectory_features_server("trajectory_features", data, features, tracks, trajectories, groupings, choices)
 }
-shinyApp(ui = ui, server = server, enableBookmarking = "server")
+# shinyApp(ui = ui, server = server, enableBookmarking = "server")
